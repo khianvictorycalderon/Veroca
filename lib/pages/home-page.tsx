c0aa554edfdc497599b8768ab5ff2ded6ded18f5
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-21 09:57:53 +0800

    Completed hero section

--- lib/pages/home-page.tsx
+++ lib/pages/home-page.tsx
@@ -1,11 +1,18 @@
 'use client'
 
 import GeneralNavbar from "../components/general-navbar"
+import { Hero } from "../components/hero"
 
 export default function HomePage() {
     return (
         <>
             <GeneralNavbar/>
+            <Hero
+                background="/images/hero-bg.png"
+                heading="Organize your items"
+                subheading="Manage your order without manually writing it and never forget client's order again!"
+                dark_cover_opacity={0.8}
+            />
         </>
     )
 }

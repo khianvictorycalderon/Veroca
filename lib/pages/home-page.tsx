7da04079480c23c4d0dc857638a77a48d5de6459
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-21 10:18:59 +0800

    Added CTA hero button

--- lib/pages/home-page.tsx
+++ lib/pages/home-page.tsx
@@ -4,6 +4,13 @@ import GeneralNavbar from "../components/general-navbar"
 import { Hero } from "../components/hero"
 
 export default function HomePage() {
+
+    const CTAButton = [
+        <button className="text-lg md:text-xl lg:text-2xl font-bold bg-green-600 px-6 py-2 rounded-md shadow hover:bg-green-500 transition duration-300 cursor-pointer">
+            GET STARTED!
+        </button>
+    ]
+
     return (
         <>
             <GeneralNavbar/>
@@ -12,6 +19,7 @@ export default function HomePage() {
                 heading="Organize your items"
                 subheading="Manage your order without manually writing it and never forget client's order again!"
                 dark_cover_opacity={0.8}
+                cta_buttons={CTAButton}
             />
         </>
     )

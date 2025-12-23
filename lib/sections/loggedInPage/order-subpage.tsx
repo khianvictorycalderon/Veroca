9d05b6c19977fdf9a503910dae1deedecf509416
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-23 08:13:58 +0800

    Colored layout structure

--- lib/sections/loggedInPage/order-subpage.tsx
+++ lib/sections/loggedInPage/order-subpage.tsx
@@ -5,16 +5,16 @@ export default function OrderSubPage() {
         <div className="lg:flex lg:flex-row w-full bg-gray-50">
             
             {/* Add Order Section + View Order */}
-            <div className="lg:flex-1/4 h-[70vh] lg:h-[100vh] flex flex-col">
-                <div className="flex-1/3 bg-red-600 overflow-y-hidden">
-                    
+            <div className="lg:flex-1/4 h-[70vh] lg:h-[100vh] flex flex-col bg-neutral-900 text-white">
+                <div className="flex-1/3 overflow-y-hidden">
+                    Add order input + button here...
                 </div>
-                <div className="flex-2/3 bg-blue-600 overflow-y-auto">
-                    
+                <div className="flex-2/3 overflow-y-auto">
+                    Orders and Search bar here...
                 </div>
             </div>
-            <div className="lg:flex-3/4 bg-green-600 h-[70vh] lg:h-[100vh] overflow-y-auto">
-                
+            <div className="lg:flex-3/4 bg-gray-100 text-neutral-700 h-[100vh] overflow-y-auto">
+                Order details here...                
             </div>
 
         </div>

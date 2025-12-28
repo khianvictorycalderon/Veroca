a97634b8333e84c296a8856447f583c27b6d9eb6
khianvictorycalderon <ckhianvictory@gmail.com>
2025-12-28 09:11:18 +0800

    Added SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,43 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const metaDesc =
+  "Manage your customer's orders with ease at Veroca, a simple and powerful order management system.";
+
 export const metadata: Metadata = {
-  title: "Veroca",
-  description: "Manage your customer's order with ease at Veroca!",
+  title: {
+    default: "Veroca",
+    template: "%s | Veroca",
+  },
+  description: metaDesc,
+  keywords: [
+    "veroca",
+    "veroca order management system",
+    "order management system",
+  ],
+  alternates: {
+    canonical: "https://veroca.vercel.app/",
+  },
+  openGraph: {
+    title: "Veroca: Order Management System",
+    description: metaDesc,
+    url: "https://veroca.vercel.app/",
+    siteName: "Veroca: Order Management System",
+    images: [
+      {
+        url: "/images/preview.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Veroca: Order Management System",
+    description: metaDesc,
+    images: ["/images/preview.png"],
+  },
 };
 
 export default function RootLayout({
